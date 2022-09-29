417eb8dac1c1cd3a7fd36d2147dac66bd34af9b2
jesse-halpern <jessehalpern1@gmail.com>
2022-09-29 13:06:48 -0400

    Removing unused noop function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
 export * from './time'
 
-export function noop() {}
-
 export function shuffle<T>(items: T[], ordering = items.map(Math.random)) {
   return items
     .map((word, i) => ({ word, order: ordering[i] }))
